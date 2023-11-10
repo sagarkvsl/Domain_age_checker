3f7cd57c4653a2706bfea7f977d2865dd4c52184
CodeSandbox <support@codesandbox.io>
2023-11-10 10:19:20 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        domain = request.form['domain']
+        api_url = f'https://www.whatsmydns.net/api/domain?q={domain}'
+        response = requests.get(api_url)
+        result_data = response.json()
+        return render_template('response.html', result=result_data)
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
